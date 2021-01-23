83743b4dc33311966e8c4d86307579bef1e97f94
Chino <chiino778@gmail.com>
2021-01-23 18:44:47 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://chiino23:KEB8A9hlGJ5BrOzl@cluster0.hkdxs.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
